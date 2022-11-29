d311a9f1b0add6e9b13d325dca290d7b84a5c63d
jUqItEr <103951167+jUqItEr@users.noreply.github.com>
2022-11-30 01:56:07 +0900

    초기 커밋

---
+++ src/kr/co/bm/base/RoundedJPasswordField.java
@@ -0,0 +1,33 @@
+package kr.co.bm.base;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.RoundRectangle2D;
+
+public class RoundedJPasswordField extends JPasswordField {
+    private final int arc = 5;
+    private Shape shape;
+
+    public RoundedJPasswordField(int size) {
+        super(size);
+        setOpaque(false);
+    }
+
+    protected void paintComponent(Graphics g) {
+        g.setColor(getBackground());
+        g.fillRoundRect(0, 0, getWidth() - 1, getHeight() - 1, arc, arc);
+        super.paintComponent(g);
+    }
+    protected void paintBorder(Graphics g) {
+        g.setColor(getForeground());
+        g.drawRoundRect(0, 0, getWidth() - 1, getHeight() - 1, arc, arc);
+    }
+
+    @Override
+    public boolean contains(int x, int y) {
+        if (shape == null || !shape.getBounds().equals(getBounds())) {
+            shape = new RoundRectangle2D.Float(0, 0, getWidth() - 1, getHeight() - 1, arc, arc);
+        }
+        return shape.contains(x, y);
+    }
+}
--- src/kr/co/bm/form/BMOwnerLoginForm.java
+++ src/kr/co/bm/form/BMOwnerLoginForm.java
@@ -1,17 +1,42 @@
 package kr.co.bm.form;
 
+import kr.co.bm.base.RoundedJPasswordField;
+import kr.co.bm.base.RoundedJTextField;
+
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 
 public class BMOwnerLoginForm extends JFrame {
-    public static void main(String[] args) {
+    private RoundedJTextField txtLoginId;
+    private JPanel pnlImageLogo;
+    private JPanel pnlMain;
+    private RoundedJPasswordField txtLoginPassword;
+    private JLabel lblId;
+    private JLabel lblPassword;
 
+    public static void main(String[] args) {
+        BMOwnerLoginForm form = new BMOwnerLoginForm();
+        form.initializeComponents();
     }
 
-    public void createUiComponents() {
-
+    public void createUIComponents() {
+        txtLoginId = new RoundedJTextField(5);
+        txtLoginPassword = new RoundedJPasswordField(5);
     }
 
     public void initializeComponents() {
+        pnlMain.setBackground(Color.WHITE);
+
+        this.setContentPane(this.pnlMain);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        this.pack();
+
+        this.setSize(300, 400);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+
 
     }
 }
